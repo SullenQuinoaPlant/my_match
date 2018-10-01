f4cfaf09add9046cad4bc67793a20d2ef9097c7c
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-01 21:32:57 +0200

    i

--- includes/mymatch.h
+++ includes/mymatch.h
@@ -1,12 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mymatch.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/01 21:31:15 by nmauvari          #+#    #+#             */
+/*   Updated: 2018/10/01 21:31:17 by nmauvari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MYMATCH_H
 # define MYMATCH_H
 
 typedef int	(*t_mtc_adder)(
 	t_mtc_node		*addee,
 	void			*pattern_store);
-	
+
 typedef int	(*t_mtc_remover)(
-	t_mtc_node
 	t_mtc_pattern	*removee_pattern,
 	void			*pattern_store);
 
@@ -14,7 +25,7 @@ typedef int	(*t_mtc_matcher)(
 	char const	**matchee_string,
 	void const	*pattern_store,
 	void		**p_ret);
-	
+
 typedef struct s_mtc_unit	t_s_mtc_unit;
 struct s_mtc_unit
 {
