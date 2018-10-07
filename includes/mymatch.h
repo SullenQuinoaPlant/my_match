baf9a50da0d215cc81c6767affbe624711d9391e
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-07 19:45:21 +0200

    i

--- includes/mymatch.h
+++ includes/mymatch.h
@@ -13,27 +13,30 @@
 #ifndef MYMATCH_H
 # define MYMATCH_H
 
-typedef void	*t_mtc_pattern;
+typedef char const	*t_mtc_pattern;
 
-typedef int	(*t_mtc_adder)(
+typedef void	*t_mtc_store;
+
+typedef int	(*t_mtc_pattern_adder)(
 	t_mtc_node		*addee,
 	t_mtc_pattern	pattern_store);
 
-typedef int	(*t_mtc_remover)(
-	t_mtc_pattern	*removee,
-	t_mtc_pattern	pattern_store);
+typedef int	(*t_mtc_pattern_remover)(
+	t_mtc_pattern	removee,
+	t_mtc_store		pattern_store)
 
-typedef int	(*t_mtc_matcher)(
+typedef int	(*t_mtc_pattern_matcher)(
 	char const		**matchee,
-	t_mtc_pattern	pattern_store,
-	void			**p_ret);
+	t_mtc_store		pattern_store,
+	t_memp			**p_ret)
 
+/*
 typedef struct s_mtc_unit	t_s_mtc_node;
 struct s_mtc_unit
 {
-	char const		*pattern;
+	char const		*node_pattern;
 	t_s_mtc_node	*previous;
-	t_mtc_pattern	continue;
+	t_mtc_store		continue;
 	t_mtc_matcher	continue_mtc;
 	t_mtc_adder		continue_add;
 	t_mtc_remover	continue_rem;
